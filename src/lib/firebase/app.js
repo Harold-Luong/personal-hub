ce122018e070b9b1e11870611bfb1bdf4503bf38
Duc Trong <ductrong16051998tn@gmail.com>
2026-07-10 09:23:03 +0700

    Setup Appcheck

--- src/lib/firebase/app.js
+++ src/lib/firebase/app.js
@@ -1,4 +1,5 @@
 import { getApp, getApps, initializeApp } from 'firebase/app'
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check'
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -21,3 +22,14 @@ if (missingConfig.length > 0) {
 export const firebaseApp = getApps().length
     ? getApp()
     : initializeApp(firebaseConfig)
+
+const recaptchaV3SiteKey = import.meta.env.VITE_RECAPTCHA_V3_SITE_KEY;
+
+if (!recaptchaV3SiteKey) {
+    throw new Error('Missing VITE_RECAPTCHA_V3_SITE_KEY. Check .env.local.')
+}
+
+export const firebaseAppCheck = initializeAppCheck(firebaseApp, {
+    provider: new ReCaptchaV3Provider(recaptchaV3SiteKey),
+    isTokenAutoRefreshEnabled: true,
+})
